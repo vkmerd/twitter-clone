ba417ac1e28069c602bcdd57d994127b900519f1
Mert Öztürk <mert32139@gmail.com>
2024-06-21 11:56:07 +0300

    login componenti olusturuldu ve ui tarafı tamamlandı

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,15 @@
 export default function Login(){
     return(
         <>
-         <div className="w-full h-screen bg-black overflow-hidden">
-                <p className="text-white">merts</p>
+         <div className="w-full h-screen bg-black overflow-hidden flex justify-center">
+            <div className="w-3/4 flex align-center">
+                <div className="basis-2/4 flex justify-center align-center">
+                <svg viewBox="0 0 24 24" aria-hidden="true" class="w-2/3"><g><path fill="#FFFFFF" d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"></path></g></svg>
+                </div>
+                <div className="basis-2/4 flex items-center">
+                    <h1 className="text-7xl text-white font-sans font-black">Happening Now!</h1>
+                </div>
+            </div>
          </div>
         </>
     )
